6701f6083687093e2568311d0326f0e1baa0ae55
ngviethoang <ngviethoang0212@gmail.com>
2022-11-14 22:39:15 +0700

    update deployment

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a992e98addc944c739b5a35bd07fdfd0-28584774.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
